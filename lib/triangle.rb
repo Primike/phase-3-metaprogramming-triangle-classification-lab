3e28fabebb7c5b339ac20e45f74f81a9c320e1a6
Prince <avecillasprince@yahoo.com>
2021-08-11 23:14:25 -0400

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+    attr_accessor :x, :y, :z
+
+    def initialize(x, y, z)
+      @x = x
+      @y = y
+      @z = z
+    end
+
+    def kind
+      sides
+      triangle_inequality
+      if x == y && x == z
+        :equilateral
+      elsif x == y || x == z || y == z
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+    def sides
+      if x <= 0 || y <= 0 || z <= 0
+        raise TriangleError
+      end
+    end
+
+    def triangle_inequality
+      if (x + y > z && x + z > y && y + z > x) == false
+        raise TriangleError
+      end
+    end
+
+    
+
+    class TriangleError < StandardError
+    end
 end
